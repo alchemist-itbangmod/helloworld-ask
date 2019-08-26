04aa3ebf285cc13ef1920e2c446dd842b6a2d831
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-08-26 17:39:39 +0700

    [ADD] _app.js and _document.js

---
+++ frontend/pages/_app.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import Head from 'next/head';
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Head>
+          <title>Hello World Ask 4.0</title>
+        </Head>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
