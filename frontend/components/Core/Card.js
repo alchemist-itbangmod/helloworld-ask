b42093772334dbdbee36b951ce93d0e6c09e0a8b
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-08-26 18:07:12 +0700

    [UPDATE] test useState

--- frontend/components/Core/Card.js
+++ frontend/components/Core/Card.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Card, Heading, Image, Box, Flex } from 'rebass';
+import { Card, Heading, Image, Box, Flex, Button } from 'rebass';
+import color from '../Theme/color';
 
 const photo = '/static/img/debugging.jpg';
 
 const CardComponent = () => {
+  const [oldWord, newWord] = React.useState('Card');
   return (
     <Flex>
-      <Box alignSelf="center" p={3} width={1 / 2} color="black" bg="primary" flex>
+      <Box alignSelf="center" p={3} width={1 / 2} color="black" flex>
         <Card width={256}>
           <Image src={photo} />
-          <Heading fontSize={[4, 5, 6]}>Card</Heading>
+          <Heading fontSize={[4, 5, 6]}>{oldWord}</Heading>
+          <Button bg="blue" color="white" onClick={() => newWord('hello')}>
+            Click
+          </Button>
         </Card>
       </Box>
     </Flex>
